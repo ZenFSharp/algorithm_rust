6c91646d055a000949da7ccccb15d9d8d925367b
zenfsharp@163.com <zenfsharp@163.com>
2021-07-14 09:32:45 +0800

    累了, 休息一天.

---
+++ lc1818/src/main.rs
@@ -0,0 +1,12 @@
+struct Solution {}
+impl Solution {
+    pub fn min_absolute_sum_diff(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        println!("累了休息一天");
+        0
+    }
+}
+
+fn main() {
+    let result = Solution::min_absolute_sum_diff(vec![1, 10, 4, 4, 2, 7], vec![9, 3, 5, 1, 7, 4]);
+    println!("{}", result);
+}
